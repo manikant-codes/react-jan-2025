d735ee1a7072c37f75cd281f496e92e510e36692
Manikant Jha <manikantcodes@gmail.com>
2025-01-21 18:26:31 +0530

    jsx

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,22 @@ import { createRoot } from "react-dom/client";
 //   React.createElement("h1", { id: "my-title" }, "Hello World!")
 // );
 
+const name = "Manikant";
+
 createRoot(document.getElementById("root")).render(
-  <h1>Sum of 5 & 5 is {5 + 5}</h1>
+  <>
+    <h1 className="my-class">Hello World!</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, debitis.
+    </p>
+    <img
+      src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
+      alt="chameleon"
+    />
+    <h1>New Title! {name}</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae,
+      molestias?
+    </p>
+  </>
 );
